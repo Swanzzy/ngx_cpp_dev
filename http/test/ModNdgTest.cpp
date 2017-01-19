ab308a8e59ab6a8a045b3076928cba4d61795f95
Swanzzy <Swanzzy@sc.com>
2017-01-20 00:57:37 +0800

    add comment 1

--- http/test/ModNdgTest.cpp
+++ http/test/ModNdgTest.cpp
@@ -4,31 +4,33 @@
 
 auto ndg_test_module = NdgTestInit::module();
 
-/*
 #include <iostream>
 
 #include "NgxAll.hpp"
 
-struct NdgTestConf final
+// 配置的数据结构
+struct NdgTestConf final						 // 禁止被继承
 {
-    ngx_flag_t enabled = NgxUnsetValue::get();
+	// ngx_flag_t 类型可以把配置文件里的 on|off 信息转换为 1|0 保存 
+    ngx_flag_t enabled = NgxUnsetValue::get();	 // 标志变量，构造时初始化
 };
 
 static void *create(ngx_conf_t* cf);
 static char *merge(ngx_conf_t *cf, void *parent, void *child);
 
-static ngx_command_t ndg_test_cmds[] =
+// 配置指令解析
+static ngx_command_t ndg_test_cmds[] =			 // 配置指令数组
 {
     {
-        ngx_string("ndg_test"),
-        NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
-        ngx_conf_set_flag_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(NdgTestConf, enabled),
-        nullptr
+        ngx_string("ndg_test"),					 // 指令的名字
+        NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,		 // 指令的作用域和类型
+        ngx_conf_set_flag_slot,					 // 解析函数指针
+        NGX_HTTP_LOC_CONF_OFFSET,				 // 数据的存储位置
+        offsetof(NdgTestConf, enabled),			 // 数据的具体存储变量
+        nullptr									 // 暂无须关心
     },
 
-    ngx_null_command
+    ngx_null_command							 // 空对象，结束数组
 };
 
 static ngx_int_t init(ngx_conf_t* cf);
@@ -62,9 +64,13 @@ ngx_module_t ndg_test_module =
     NGX_MODULE_V1_PADDING
 };
 
+/* *
+ * Nginx要求模块自己分配配置数据结构的内存
+ * */
+// 创建配置数据
 static void* create(ngx_conf_t* cf)
-{
-    return NgxPool(cf).alloc<NdgTestConf>();
+{												
+    return NgxPool(cf).alloc<NdgTestConf>();	 // 由内存池分配内存，构造对象	
 }
 
 static char *merge(ngx_conf_t *cf, void *parent, void *child)
@@ -111,5 +117,4 @@ static ngx_int_t handler(ngx_http_request_t *r)
 
     return NGX_DECLINED;
 }
-*/
 
